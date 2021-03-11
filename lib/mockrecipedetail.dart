3a0070648075fc9b9ecc2a295d3c8620576c0981
mswift42 <martin.haesler@gmail.com>
2021-03-11 10:35:47 +0000

    add mockrecipedetail.dart.

---
+++ lib/mockrecipedetail.dart
@@ -0,0 +1,35 @@
+import 'package:flck/recipe.dart';
+
+var grueneImSpeckMantel = RecipeDetail(
+  title: "Grüne Bohnen im Speckmantel",
+  rating: "4.49",
+  difficulty: "simpel",
+  cookingtime: "ca. 15 Min.",
+  thumbnail:
+      "https://static.chefkoch-cdn.de/ck.de/rezepte/56/56345/1124631-420x280-fix-gruene-bohnen-im-speckmantel.jpg",
+  ingredients: [RecipeIngredient("800\u00a0g", "Bohnen, frische")],
+  method:
+      "Bohnen waschen und die Spitzen abschneiden.\nBohnenkraut, Knoblauch, zerdrückte Pfefferkörner und Salz mit Öl kurz anrösten. 2 Liter Wasser zugießen, 10 Min. kochen, durchsieben. Diese Brühe aufkochen und die Bohnen in 3 Portionen nacheinander sprudelnd garen. Schnell in kaltem Wasser abkühlen, in einem Tuch abtrocknen.\n\nBohnen in Bacon einwickeln. Butter in einer feuerfesten Form erhitzen, die Bohnen reingeben (mit der Specknaht nach unten) und zugedeckt im Ofen bei 180 °C - 200 °C erhitzen (ca. 5 Minuten), dabei einmal wenden.",
+);
+
+var schupfnudel = RecipeDetail(
+  title: "Schupfnudel - Bohnen - Pfanne",
+  rating: "4.37",
+  difficulty: "normal",
+  cookingtime: "",
+  thumbnail:
+      "https://static.chefkoch-cdn.de/ck.de/rezepte/117/117138/1156413-420x280-fix-schupfnudel-bohnen-pfanne.jpg",
+  ingredients: [
+    RecipeIngredient("500\u00a0g", "Schupfnudeln (Kühlregal)"),
+    RecipeIngredient("200\u00a0g", "Schinken, gekochter"),
+    RecipeIngredient("250\u00a0g", "Bohnen (Prinzessbohnen, TK)"),
+    RecipeIngredient("1/8\u00a0Liter", "Fleischbrühe"),
+    RecipeIngredient("1\u00a0Becher", "Crème fraîche"),
+    RecipeIngredient(
+        "4\u00a0Scheibe/n", "Käse (Toast-Käse, z.B. Scheibletten)"),
+    RecipeIngredient("\u00a0n. B.", "Salz und Pfeffer"),
+    RecipeIngredient("\u00a0", "Olivenöl"),
+  ],
+  method:
+      "Die Prinzessböhnchen für ca. 5 Min. in kochendem Wasser garen. \n\nDen Kochschinken würfeln und mit etwas Olivenöl in der Pfanne anbraten. Die Schupfnudeln hinzugeben und 5-8 Min. zusammen mit dem Schinken braten, bis die Schupfnudeln eine goldgelbe Farbe annehmen. Die Prinzessbohnen hinzu geben. Nun 1/8 l Fleischbrühe zugießen und mit Crème fraiche nach Belieben andicken. Nach Geschmack würzen. Als Abschluss die Käsescheiben oben auflegen, bis diese verlaufen. Sofort servieren.",
+);
