c610c4647d2dac543d3d376e74c7fd8fe5009bb4
mswift42 <martin.haesler@gmail.com>
2021-03-27 08:46:25 +0000

    delete run method.

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -5,13 +5,11 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flck/main.dart' as app;
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 
-import 'package:flck/main.dart' as app;
-
-void main() => run(_testMain);
+void main() => _testMain();
 
 void _testMain() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
